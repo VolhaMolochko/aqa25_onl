198b5f58df2f2008d5dc4647768f4910c495a3e2
Volha Milk <olkamiroyka@gmail.com>
2024-03-01 17:37:49 +0300

    fix 1

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -3,7 +3,16 @@ public class Calculator {
         return a + b;
     }
 
+    public int sum(int a, int b, String nessage) {
+        System.out.println(nessage);
+        return a + b;
+    }
+
     public double div(double c, double d) {
         return c / d;
+    }
+
+    public double div(int c, int d) {
+        return (double) c / d;
     }
 }
