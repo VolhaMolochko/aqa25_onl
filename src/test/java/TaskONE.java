70275aa10f08a97e21598b1667741689b0439eba
Volha Milk <olkamiroyka@gmail.com>
2024-03-02 20:44:36 +0300

    1 and 2 task

---
+++ src/test/java/TaskONE.java
@@ -0,0 +1,58 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebElement;
+
+
+public class TaskONE {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new Core.BrowserService().getDriver();
+    }
+
+    @Test (testName = "Heating floor test")
+    public void calkRehau() throws InterruptedException {
+        driver.get("https://kermi-fko.ru/raschety/Calc-Rehau-Solelec.aspx");
+        WebElement width = driver.findElement(By.id("el_f_width"));                            //ввожу размеры
+        width.sendKeys("6");
+
+        WebElement length = driver.findElement(By.id("el_f_lenght"));
+        length.sendKeys("4");
+
+        WebElement selectWebElementRoom = driver.findElement(By.id("room_type"));            //работаю с выпадающим списком
+        Select selectRoom = new Select(selectWebElementRoom);
+
+        selectRoom.selectByValue("2");
+        Thread.sleep(1000);
+
+        WebElement selectWebElementHeatingType = driver.findElement(By.id("heating_type"));
+        Select selectHeatingType = new Select(selectWebElementHeatingType);
+
+        selectHeatingType.selectByValue("3");
+
+        WebElement button = driver.findElement(By.name("button"));                               //нажимаю "Рассчитать"
+        button.click();
+
+        Thread.sleep(2000);
+
+        WebElement resultPower = driver.findElement(By.id("floor_cable_power"));                  //отсюда и ниже проверяю значения которые посчитались
+        WebElement resultSpecificPower = driver.findElement(By.id("spec_floor_cable_power"));
+
+        Assert.assertEquals(resultPower.getAttribute("value"), "1526");
+        Assert.assertEquals(resultSpecificPower.getAttribute("value"), "64");
+
+        Thread.sleep(2000);
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+        driver.quit();
+    }
+}
---
+++ src/test/java/TaskTwo.java
@@ -0,0 +1,67 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class TaskTwo {
+    private WebDriver driver;
+
+    @BeforeMethod
+    public void setup() {
+        driver = new Core.BrowserService().getDriver();
+    }
+
+    @Test(testName = "Laminat calc test")
+    public void laminatCalcTest() throws InterruptedException {
+        driver.get("https://calc.by/building-calculators/laminate.html");
+
+        WebElement roomLength = driver.findElement(By.id("ln_room_id"));
+        roomLength.clear();
+        roomLength.sendKeys("500");
+
+        WebElement roomWidth = driver.findElement(By.id("wd_room_id"));
+        roomWidth.clear();
+        roomWidth.sendKeys("400");
+
+        WebElement lengthLaminatePanel = driver.findElement(By.id("ln_lam_id"));
+        lengthLaminatePanel.clear();
+        lengthLaminatePanel.sendKeys("2000");
+
+        WebElement widthLaminatePanel = driver.findElement(By.id("wd_lam_id"));
+        widthLaminatePanel.clear();
+        widthLaminatePanel.sendKeys("200");
+
+        WebElement selectWebElementLayingMethod = driver.findElement(By.id("laying_method_laminate"));    //выпадающий список
+        Select selectLayingMethod = new Select(selectWebElementLayingMethod);
+
+        selectLayingMethod.selectByValue("0");
+        Thread.sleep(1000);
+
+
+        WebElement radio = driver.findElement(By.id("direction-laminate-id1"));     //радиоботом
+        radio.click();
+        Thread.sleep(1000);
+
+        WebElement button = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[1]/div[3]/article/section/div[2]/div[2]/div[2]"));
+        button.click();
+
+        Thread.sleep(2000);
+
+        WebElement numberOfBoards = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[1]/div[3]/article/section/div[2]/div[3]/div[2]/div[1]/span"));
+        Assert.assertEquals(numberOfBoards.getText(), "51");
+
+        WebElement numberOfPackages = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[1]/div[3]/article/section/div[2]/div[3]/div[2]/div[2]/span"));
+        Assert.assertEquals(numberOfPackages.getText(), "7");
+
+    }
+
+    @AfterMethod
+    public void tearDown() {
+
+        driver.quit();
+    }
+}
