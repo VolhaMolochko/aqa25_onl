eae959704e37e6e73801e82040f53b1f96502b9d
Volha Milk <olkamiroyka@gmail.com>
2024-03-02 20:55:21 +0300

    2 task Upd

--- src/test/java/TaskTwo.java
+++ src/test/java/TaskTwo.java
@@ -1,4 +1,5 @@
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
@@ -18,20 +19,26 @@ public class TaskTwo {
     @Test(testName = "Laminat calc test")
     public void laminatCalcTest() throws InterruptedException {
         driver.get("https://calc.by/building-calculators/laminate.html");
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
         WebElement roomLength = driver.findElement(By.id("ln_room_id"));
+
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", roomLength);
         roomLength.clear();
         roomLength.sendKeys("500");
 
         WebElement roomWidth = driver.findElement(By.id("wd_room_id"));
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", roomWidth);
         roomWidth.clear();
         roomWidth.sendKeys("400");
 
         WebElement lengthLaminatePanel = driver.findElement(By.id("ln_lam_id"));
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", lengthLaminatePanel);
         lengthLaminatePanel.clear();
         lengthLaminatePanel.sendKeys("2000");
 
         WebElement widthLaminatePanel = driver.findElement(By.id("wd_lam_id"));
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", widthLaminatePanel);
         widthLaminatePanel.clear();
         widthLaminatePanel.sendKeys("200");
 
@@ -43,19 +50,23 @@ public class TaskTwo {
 
 
         WebElement radio = driver.findElement(By.id("direction-laminate-id1"));     //радиоботом
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", radio);
         radio.click();
         Thread.sleep(1000);
 
         WebElement button = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[1]/div[3]/article/section/div[2]/div[2]/div[2]"));
+        js.executeScript("arguments[0].scrollIntoView({block: 'center'});", button);
         button.click();
 
-        Thread.sleep(2000);
+        Thread.sleep(3000);
 
         WebElement numberOfBoards = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[1]/div[3]/article/section/div[2]/div[3]/div[2]/div[1]/span"));
         Assert.assertEquals(numberOfBoards.getText(), "51");
 
         WebElement numberOfPackages = driver.findElement(By.xpath("/html/body/div[2]/div[2]/div/div[1]/div[3]/article/section/div[2]/div[3]/div[2]/div[2]/span"));
         Assert.assertEquals(numberOfPackages.getText(), "7");
+
+        Thread.sleep(2000);
 
     }
 
