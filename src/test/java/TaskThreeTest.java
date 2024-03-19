d670dfd83276c4af35d52a74230cd427f009f896
Volha Milk <olkamiroyka@gmail.com>
2024-03-19 10:31:21 +0300

    task 3

--- src/test/java/TaskThreeTest.java
+++ src/test/java/TaskThreeTest.java
@@ -5,24 +5,24 @@ import org.testng.annotations.Test;
 
 import java.io.File;
 
-public class TaskThreeTest  extends BaseTest {
+public class TaskThreeTest extends BaseTest {
 //    Добавить тест для страницы File Upload
 //    Загрузить файл
 //    Проверить, что имя файла на странице совпадает с именем загруженного файла
 
     @Test
-    public void FileUploadTest(){
+    public void FileUploadTest() {
         driver.get("https://the-internet.herokuapp.com/upload");
 
         WebElement fileUploadElement = waitsService.waitForExists(By.xpath("//*[@name ='file']"));
         String pathToFile = TaskThreeTest.class.getClassLoader().getResource("Pearl.txt").getPath().substring(1);
         System.out.println(pathToFile);
 
-       fileUploadElement.sendKeys(pathToFile);
-//        waitsService.waitForExists(By.id("file-submit")).submit();
-//
-//        WebElement checkNameFile = waitsService.waitForExists(By.id("uploaded-files"));
-//        Assert.assertEquals(checkNameFile.getText(),"Pearl.txt" );
+        fileUploadElement.sendKeys(pathToFile);
+        waitsService.waitForExists(By.id("file-submit")).submit();
+
+        WebElement checkNameFile = waitsService.waitForExists(By.id("uploaded-files"));
+        Assert.assertEquals(checkNameFile.getText(), "Pearl.txt");
     }
 }
 
